2e90f6c3543ff1be80a4f019d10660d6a54decb7
luminion <604647004@qq.com>
2025-12-19 22:53:54 +0800

    feat(aop): 启用AOP自动配置并添加限流功能
    - 移除基于属性的AOP启用条件
    - 添加RateLimitAspect切面配置
    - 配置MethodFingerprinter用于方法签名生成
    - 配置JdkRateLimiter作为默认限流实现
    - 添加@Order注解确保限流器优先级
    - 删除AopProperties配置类

--- autoconfigure-core/src/main/java/io/github/luminion/autoconfigure/aop/AopAutoConfiguration.java
+++ autoconfigure-core/src/main/java/io/github/luminion/autoconfigure/aop/AopAutoConfiguration.java
@@ -1,12 +1,19 @@
 package io.github.luminion.autoconfigure.aop;
 
-import io.github.luminion.autoconfigure.ConfigKey;
+import io.github.luminion.autoconfigure.aop.aspect.RateLimitAspect;
+import io.github.luminion.autoconfigure.aop.core.MethodFingerprinter;
+import io.github.luminion.autoconfigure.aop.core.RateLimiter;
+import io.github.luminion.autoconfigure.aop.support.ratelimit.JdkRateLimiter;
+import io.github.luminion.autoconfigure.aop.support.signature.SpELMethodFingerprinter;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.weaver.Advice;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 
 /**
  * AOP（面向切面编程）自动配置
@@ -17,31 +24,32 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @Slf4j
 @AutoConfiguration
 @ConditionalOnClass({Advice.class})
-@ConditionalOnProperty(value = ConfigKey.AOP_ENABLE, havingValue = "true", matchIfMissing = true)
-@EnableConfigurationProperties({AopProperties.class})
 public class AopAutoConfiguration {
 
-    //@Bean
-    //@ConditionalOnMissingBean(RateLimitAspect.class)
-    //@ConditionalOnBean({MethodFingerprinter.class, RateLimiter.class})
-    //public RateLimitAspect methodLimitAspect(BeanFactory beanFactory) {
-    //    RateLimitAspect rateLimitAspect = new RateLimitAspect(beanFactory);
-    //    log.debug("RateLimitAspect Configured");
-    //    return rateLimitAspect;
-    //}
-    //
-    //@Bean
-    //@ConditionalOnMissingBean(MethodFingerprinter.class)
-    //public MethodFingerprinter spelSignatureProvider(AopProperties aopProperties) {
-    //    log.debug("SpELMethodFingerprinter Configured");
-    //    return new SpELMethodFingerprinter(aopProperties.getMethodLimitPrefix());
-    //}
-    //
-    //@Bean
-    //@ConditionalOnMissingBean(RateLimiter.class)
-    //public RateLimiter redisRateLimiter() {
-    //    log.debug("JdkRateLimiter Configured");
-    //    return new JdkRateLimiter();
-    //}
+    @Bean
+    @ConditionalOnMissingBean(RateLimitAspect.class)
+    @ConditionalOnBean({MethodFingerprinter.class, RateLimiter.class})
+    public RateLimitAspect methodLimitAspect(BeanFactory beanFactory) {
+        RateLimitAspect rateLimitAspect = new RateLimitAspect(beanFactory);
+        log.debug("RateLimitAspect Configured");
+        return rateLimitAspect;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(MethodFingerprinter.class)
+    public MethodFingerprinter spelSignatureProvider() {
+        log.debug("SpELMethodFingerprinter Configured");
+        return new SpELMethodFingerprinter("sp_e_l_method_fingerprinter");
+    }
+
+    
+    
+    @Bean
+    @ConditionalOnMissingBean(RateLimiter.class)
+    @Order(100)
+    public RateLimiter redisRateLimiter() {
+        log.debug("JdkRateLimiter Configured");
+        return new JdkRateLimiter();
+    }
 
 }
