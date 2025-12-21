c53c712c06cc76c59d63e55eca44a761847e1f6a
luminion <604647004@qq.com>
2025-12-21 22:52:38 +0800

    feat(jackson): 优化Redis序列化配置并增强Jackson自动配置
    - 引入JavaTimeModule以支持JDK8时间序列化
    - 配置ObjectMapper以忽略未知属性和空对象序列化异常
    - 启用类型信息序列化以支持多态类型
    - 注册null值序列化器以处理Redis中的null值
    - 将spring-data-redis依赖设为可选
    - 添加自动配置元数据文件
    - 移除不必要的条件注解依赖
    - 优化数值类型转字符串的序列化逻辑

--- autoconfigure-jackson/src/main/java/io/github/luminion/autoconfigure/jackson/JacksonAutoConfiguration.java
+++ autoconfigure-jackson/src/main/java/io/github/luminion/autoconfigure/jackson/JacksonAutoConfiguration.java
@@ -1,10 +1,14 @@
 package io.github.luminion.autoconfigure.jackson;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
@@ -14,7 +18,6 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import io.github.luminion.autoconfigure.DateTimeFormatProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -23,6 +26,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -89,18 +93,18 @@ public class JacksonAutoConfiguration {
                     generator:
                       write_numbers_as_strings: true #序列化的时候，将数值类型全部转换成字符串返回
                  */
-                if (jacksonProperties.isLongToString()){
+                if (jacksonProperties.isLongToString()) {
                     builder.serializerByType(Long.class, ToStringSerializer.instance);
                     builder.serializerByType(Long.TYPE, ToStringSerializer.instance);
                 }
-                if (jacksonProperties.isDoubleToString()){
+                if (jacksonProperties.isDoubleToString()) {
                     builder.serializerByType(Double.class, ToStringSerializer.instance);
                     builder.serializerByType(Double.TYPE, ToStringSerializer.instance);
                 }
-                if (jacksonProperties.isBigIntegerToString()){
+                if (jacksonProperties.isBigIntegerToString()) {
                     builder.serializerByType(BigInteger.class, ToStringSerializer.instance);
                 }
-                if (jacksonProperties.isBigDecimalToString()){
+                if (jacksonProperties.isBigDecimalToString()) {
                     builder.serializerByType(BigDecimal.class, ToStringSerializer.instance);
                 }
 
@@ -123,16 +127,26 @@ public class JacksonAutoConfiguration {
     }
     
     @Configuration(proxyBeanMethods = false)
+    @ConditionalOnClass(RedisTemplate.class)
     static class JacksonRedisConfiguration{
         @Bean
-        @ConditionalOnBean(Jackson2ObjectMapperBuilder.class)
-        @ConditionalOnMissingBean
-        public RedisSerializer<Object> redisSerializer(Jackson2ObjectMapperBuilder builder) {
-            log.debug("RedisSerializer Configured");
-            ObjectMapper objectMapper = builder.build();
-            //启用反序列化所需的类型信息,在属性中添加@class
-            objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-            //配置null值的序列化器
+        @ConditionalOnMissingBean(name = "redisSerializer")
+        public RedisSerializer<Object> redisSerializer() {
+            ObjectMapper objectMapper = JsonMapper.builder().disable(MapperFeature.USE_ANNOTATIONS).build();
+            // 反序列化时候遇到不匹配的属性并不抛出异常
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            // 序列化时候遇到空对象不抛出异常
+            objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+            // 反序列化的时候如果是无效子类型,不抛出异常
+            objectMapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false);
+            // 不使用默认的dateTime进行序列化,
+            objectMapper.configure(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
+            // 使用JSR310提供的序列化类,里面包含了大量的JDK8时间序列化类
+            objectMapper.registerModule(new JavaTimeModule());
+            // 启用反序列化所需的类型信息,在属性中添加@class
+            objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL,
+                    JsonTypeInfo.As.PROPERTY);
+            // 配置null值的序列化器
             GenericJackson2JsonRedisSerializer.registerNullValueSerializer(objectMapper, null);
             return new GenericJackson2JsonRedisSerializer(objectMapper);
         }
